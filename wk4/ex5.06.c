4914117b933f09601f1f7ed991e36d7dc42129f8
jw426 <jiwonbae010426@gmail.com>
2024-08-12 11:54:34 +1000

    better skeleton

---
+++ wk4/ex5.06.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+
+    // main headquarters!
+
+    return 0;
+}
+
+int amicable_pair(int n1, int n2) {
+
+    // return value should be
+    // 1 if amicable
+    // 0 if else
+}
+
+int sum_factors(int num) {
+
+
+    // return value should be int
+}
