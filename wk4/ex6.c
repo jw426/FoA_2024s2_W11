129b41472039772c85b6714df40170bdb2facc1c
jw426 <jiwonbae010426@gmail.com>
2024-08-12 12:21:03 +1000

    d

---
+++ wk4/ex6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+
+    
+    return 0;
+}
+
+int try_one_coin(int *cents, int coin) {
+    
+    
+}
+
+void print_change(int cents) {
+ 
+ 
+}
+
+int round_to_5(int cents) {
+
+    
+}
+    
